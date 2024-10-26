68911ccf5fdf1492fba436393b973ba8815ed6ce
Rich Smith <rich.smith@sheridancollege.ca>
2024-10-26 08:42:46 -0400

    initial project commit

---
+++ src/main/java/com/example/linkedlist/LinkedlistApplication.java
@@ -0,0 +1,13 @@
+package com.example.linkedlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedlistApplication.class, args);
+	}
+
+}
